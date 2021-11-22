c080cfaa47548cf7d8858b1a5202ef87b169ed04
shaikhreza43 <reza.rk786@gmail.com>
2021-11-22 18:12:43 +0530

    general changes

--- src/components/CoursesCard.js
+++ src/components/CoursesCard.js
@@ -3,47 +3,164 @@ import React from "react";
 
 export default function CoursesCard(props) {
   return (
-    <div className="outer-div-courses">
-      <div className="card" style={{ width: "10rem",display:'flex',justifyContent:'center',alignItems:'center',padding:'8px' }}>
-        <p>9</p>
-        <div className="card-body">
-          <p className="card-text">
-            Class IX
-          </p>
+    <>
+      <div className="outer-div-courses">
+        <div
+          className="card"
+          style={{
+            width: "10rem",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            padding: "8px",
+          }}
+        >
+          <h5>9</h5>
+          <div className="card-body">
+            <p className="card-text">Class IX</p>
+          </div>
+        </div>
+        <div
+          className="card"
+          style={{
+            width: "10rem",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            padding: "8px",
+          }}
+        >
+          <h5>10</h5>
+          <div className="card-body">
+            <p className="card-text">Class X</p>
+          </div>
+        </div>
+        <div
+          className="card"
+          style={{
+            width: "10rem",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            padding: "8px",
+          }}
+        >
+          <h5>11</h5>
+          <div className="card-body">
+            <p className="card-text">Class XI</p>
+          </div>
+        </div>
+        <div
+          className="card"
+          style={{
+            width: "10rem",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            padding: "8px",
+          }}
+        >
+          <h5>12</h5>
+          <div className="card-body">
+            <p className="card-text">Class XII</p>
+          </div>
+        </div>
+        <div
+          className="card"
+          style={{
+            width: "10rem",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            padding: "8px",
+          }}
+        >
+          <h5>1-8</h5>
+          <div className="card-body">
+            <p className="card-text">Class 1-8</p>
+          </div>
         </div>
       </div>
-      <div className="card" style={{ width: "10rem",display:'flex',justifyContent:'center',alignItems:'center',padding:'8px' }}>
-        <p>10</p>
-        <div className="card-body">
-          <p className="card-text">
-            Class X
-          </p>
+      {/* New ROw */}
+      <div className="row mt-5">
+        <div className="outer-div-courses">
+          <div
+            className="card"
+            style={{
+              width: "10rem",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              padding: "8px",
+            }}
+          >
+            <b>IIT</b>
+            <div className="card-body">
+              <p className="card-text">IIT JEE</p>
+            </div>
+          </div>
+          <div
+            className="card"
+            style={{
+              width: "10rem",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              padding: "8px",
+            }}
+          >
+            <b>NEET</b>
+            <div className="card-body">
+              <p className="card-text">NEET</p>
+            </div>
+          </div>
+          <div
+            className="card"
+            style={{
+              width: "10rem",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              padding: "8px",
+            }}
+          >
+            <b>ICSE</b>
+            <div className="card-body">
+              <p className="card-text">ICSE</p>
+            </div>
+          </div>
+          <div
+            className="card"
+            style={{
+              width: "10rem",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              padding: "8px",
+            }}
+          >
+            <b>Govt. Exam</b>
+            <div className="card-body">
+              <p className="card-text">GOVT.</p>
+            </div>
+          </div>
+          <div
+            className="card"
+            style={{
+              width: "10rem",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              padding: "8px",
+            }}
+          >
+            <b>Engg.</b>
+            <div className="card-body">
+              <p className="card-text">GOVT</p>
+            </div>
+          </div>
         </div>
       </div>
-      <div className="card" style={{ width: "10rem",display:'flex',justifyContent:'center',alignItems:'center',padding:'8px' }}>
-        <p>11</p>
-        <div className="card-body">
-          <p className="card-text">
-            Class XI
-          </p>
-        </div>
-      </div>
-      <div className="card" style={{ width: "10rem",display:'flex',justifyContent:'center',alignItems:'center',padding:'8px' }}>
-        <p>12</p>
-        <div className="card-body">
-          <p className="card-text">
-            Class XII
-          </p>
-        </div>
-      </div>
-      <div className="card" style={{ width: "10rem",display:'flex',justifyContent:'center',alignItems:'center',padding:'8px' }}>
-        <p>1-8</p>
-        <div className="card-body">
-          <p className="card-text">
-            Class 1-8
-          </p>
-        </div>
-      </div>
-    </div>
+    </>
   );
 }
